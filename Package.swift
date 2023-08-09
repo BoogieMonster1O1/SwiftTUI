d6c860487a08919e9dade7521c7a226709c0d07e
Shrish Deshpande <shrish.deshpande@icloud.com>
2023-08-09 11:41:47 +0530

    disable macos only

--- Package.swift
+++ Package.swift
@@ -4,9 +4,6 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftTUI",
-    platforms: [
-        .macOS(.v11)
-    ],
     products: [
         .library(
             name: "SwiftTUI",
